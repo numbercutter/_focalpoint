6146e42e8f9d5fc97e6f4c580e91282d363dbaef
Mitch <jamlets@protonmail.com>
2023-05-12 07:47:18 -0400

    key

--- focal/src/components/Bookshelf.js
+++ focal/src/components/Bookshelf.js
@@ -5,8 +5,8 @@ const Bookshelf = ({ books }) => {
     return (
         <Grid templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)"]} gap={6}>
             {books.map((book, i) => (
-                <Box as="a" w="100%" p="4" rounded="md" boxShadow="lg" overflow="hidden">
-                    <Link href={book.file} key={i} passHref>
+                <Box key={i} as="a" w="100%" p="4" rounded="md" boxShadow="lg" overflow="hidden">
+                    <Link href={book.file} passHref>
                         <Box bg="brand.200" color="brand.300" p="2" rounded="md" border="2px" borderColor="brand.100">
                             <AspectRatio ratio={5.5 / 8.5} maxH="800px">
                                 <Image src={book.cover} alt={book.title} objectFit="contain" />
