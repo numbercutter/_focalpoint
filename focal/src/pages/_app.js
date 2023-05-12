60b059f7542ea34e6d9a392ea252284ef3e35b5b
Mitch <jamlets@protonmail.com>
2023-05-12 07:39:05 -0400

    remove conflix

--- focal/src/pages/_app.js
+++ focal/src/pages/_app.js
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
-import { ChakraProvider } from "@chakra-ui/react"
-import theme from '../themes/theme'
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from '../themes/theme';
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
-=======
-// src/pages/_app.js
-
-import '../styles/global.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
->>>>>>> 6721592a72516155befebbe9c72f63c69669b774
+  );
 }
 
-export default MyApp
+export default MyApp;
